a21b57d0c0560f1b004638362f6e777a58fe8803
dms-akshat <120497443+dms-akshat@users.noreply.github.com>
2023-04-17 11:23:10 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote>  quotes = [
+    Quote(text:'Just Do IT',author: 'NIKE'),
+    Quote(text:'All wars are won or lost before they are ever fought',author: 'Sun Tzu'),
+    Quote(text: 'The greatest victory is that which requires no battle', author: 'Sun Tzu'),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[400],
+      appBar: AppBar(
+        title: Text(
+          'THE BLADE QUOTES',
+          style: TextStyle(
+            wordSpacing: 3,
+            letterSpacing: 1,
+          )
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.pink[700],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: quotes.map((quote) => QuoteCard(
+            quote: quote,
+            delete: () {
+              setState(() {
+                quotes.remove(quote);
+              });
+              return 0;
+            }
+          )).toList()
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/quote_card.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  final Function delete;
+  QuoteCard({required this.quote, required this.delete});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16,16,16,0),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black
+              )
+              ),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey[600]
+              )
+              ),
+            SizedBox(height: 10,),
+            TextButton.icon(
+              onPressed: delete,
+              label: Text(
+                'DELETE',
+                style: TextStyle(
+                  color: Colors.purple[800],
+                )),
+              icon: Icon(
+                Icons.delete_rounded,
+                color: Colors.purple[800],
+              ),
+
+            )  
+          ],
+        ),
+      )
+    );
+  }
+}
